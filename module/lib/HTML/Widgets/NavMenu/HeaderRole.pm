45d9285340922fa6a8651dc9ee47595f05a0c1f4
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2004-12-25 18:08:31 +0000

    Nav-Menu:
    Added documentation for HTML::Widgets::NavMenu::HeaderRole.

--- module/lib/HTML/Widgets/NavMenu/HeaderRole.pm
+++ module/lib/HTML/Widgets/NavMenu/HeaderRole.pm
@@ -18,3 +18,44 @@ sub get_nav_menu_traverser
 
 1;
 
+__END__
+
+=head1 NAME
+
+HTML::Widgets::NavMenu::HeaderRole - A Specialized HTML::Widgets::NavMenu 
+sub-class
+
+=head1 SYNOPOSIS
+
+Mostly the same as L<HTML::Widgets::NavMenu> except for the ability to
+specify C<'role' =E<gt> "header"> as one of the node attributes.
+
+=head1 DESCRIPTION
+
+This module is constructed and invoked similarly to HTML::Widgets::NavMenu.
+The only difference is that it is meaningful to specify C<"header"> as the
+value of the C<'role'>.
+
+In that case, the link or bolded label will be rendered within its own
+C<E<lt>h2E<gt>> header. The HTML will look something like this:
+
+    </ul>
+    <h2>
+    <a href="../me/" title="About Myself">About Me</a>
+    </h2>
+    <ul>
+
+An example of this use can be found in the Perl Beginners Site 
+( L<http://perl-begin.berlios.de/> ).
+
+=head1 SEE ALSO
+
+L<HTML::Widgets::NavMenu> for the complete documentation of the super-class.
+
+=head1 AUTHORS
+
+Shlomi Fish E<lt>shlomif@iglu.org.ilE<gt> 
+(L<http://search.cpan.org/~shlomif/>).
+
+=cut 
+
