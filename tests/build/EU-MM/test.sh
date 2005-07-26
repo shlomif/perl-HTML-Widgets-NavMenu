c22f51b9f5b8db4a4522266990b560b382b034bc
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-07-26 17:26:37 +0000

    Added a regression test for the ExtUtils::MakeMaker-based build system.

---
+++ tests/build/EU-MM/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd ../../../module
+rm -f Build
+if ! perl Makefile.PL ; then
+    echo "perl Makefile.PL failed" 1>&2
+    exit 1    
+fi
+if ! make ; then
+    echo "make failed" 1>&2
+    exit 1
+fi
+if ! make test ; then
+    echo "make test failed" 1>&2
+    exit 1
+fi
