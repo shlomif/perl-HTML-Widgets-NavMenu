9bc6db6690359ee19da5c8c0b68e02044268eb1b
Shlomi Fish <shlomif@shlomifish.org>
2020-01-14 14:24:21 +0200

    update tag-release.pl

--- HTML-Widgets-NavMenu-ToJSON/scripts/tag-release.pl
+++ HTML-Widgets-NavMenu-ToJSON/scripts/tag-release.pl
@@ -3,24 +3,22 @@
 use strict;
 use warnings;
 
-use IO::All;
+use IO::All qw/ io /;
 
 my ($version) =
-    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file("./lib/HTML/Widgets/NavMenu/ToJSON.pm")->getlines()
-    )
-    ;
+    ( map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
+        io->file("./lib/HTML/Widgets/NavMenu/ToJSON.pm")->getlines() );
 
-if (!defined ($version))
+if ( !defined($version) )
 {
     die "Version is undefined!";
 }
 
 my @cmd = (
-    "hg", "tag", "-m",
+    "git", "tag", "-m",
     "Tagging HTML-Widgets-NavMenu-ToJSON as $version",
     "cpan-releases/HTML-Widgets-NavMenu-ToJSON-v$version",
 );
 
-print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
 exec(@cmd);
